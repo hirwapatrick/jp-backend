987d1d2afa6b65df6f4e190bb708fbc590557bad
hirwapatrick <hirwap96@gmail.com>
2026-04-06 18:31:36 +0200

    Latest Commit

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ const app = express();
 // CORS CONFIGURATION - FIXED
 // ============================================
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+  origin: ['http://localhost:5173', 'http://127.0.0.1:5173', 'https://jacques-photographer.vercel.app'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
